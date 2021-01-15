c4d44c2d94002e8cf1b966ee0ed87df4636f480e
ginabagi <415308@student.fontys.nl>
2021-01-15 15:15:06 +0100

    cards update

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import Carditem from './Carditem';
+import './Cards.css';
 
 function Cards() {
     return (
@@ -7,7 +9,11 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        
+                        <Carditem
+                        src="images/img-9.jpg"
+                        text="explore the hidden waterfall deep inside the Amazon jungle"
+                        label="Adventure"
+                        path="/services" />
                     </ul>
                 </div>
             </div>
