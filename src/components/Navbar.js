a59776691ab1dfe81d4fc8211436b943ecc17a30
ginabagi <415308@student.fontys.nl>
2021-01-15 09:59:45 +0100

    navigation css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from './Button';
-import "./Navbar.css";
+import "../components/Navbar.css";
 
 function Navbar() {
     const [click, setClick] = useState(false);
@@ -22,7 +22,7 @@ function Navbar() {
 
     return (
         <>
-        <nav className="Navbar">
+        <nav className="navbar">
             <div className="navbar-container">
                <Link to="/" className="navbar-logo">
                    NXT TRVL <i className="fab fa-typo3"/>
