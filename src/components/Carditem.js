57c40938fbd18bff02a369a749c0f307102dfa14
ginabagi <415308@student.fontys.nl>
2021-01-17 16:56:36 +0100

    cards adjustment

--- src/components/Carditem.js
+++ src/components/Carditem.js
@@ -8,16 +8,15 @@ function CardItem(props) {
             <li className="cards__item">
                 <Link className="cards__item___link" to={props.path}>
                     <figure className="cards__item__pic-wrap" data-category={props.label}>
-                        <img src={props.src} alt="Travel Image" className="cards__item__img"/>
+                        <img src={props.src} alt="Travel img" className="cards__item__img"/>
                     </figure>
                     <div className="cards__item__info">
-                        <h5 className="cards__item__text">{props.text} </h5>
-                        
+                        <h5 className="cards__item__text">{props.text}</h5> 
                     </div>
                 </Link>
             </li>
         </>
-    )
+    );
 }
 
 export default CardItem;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,8 +29,8 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            NXT TRVL
-            <i className='fab fa-typo3' />
+            NXT TRVL 
+            <i className="fab fa-typo3" style={{marginLeft:".5rem"}}/>
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
